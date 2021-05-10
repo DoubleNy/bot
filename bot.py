3f7a3f10fa2c92eee7bdf24359a2d1ef82d20b46
Cristian Ninicu <cristian@shoreline.io>
2021-05-11 01:44:58 +0300

    update

--- bot.py
+++ bot.py
@@ -70,6 +70,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("piracy", piracy))
     dp.add_handler(CommandHandler("price", price))
+    dp.add_handler(CommandHandler("p", price))
     # log all errors
     dp.add_error_handler(error)
 
