97b3da507108e0040bd8ff97c9169cfc98b5bb98
Cristian Ninicu <cristian@shoreline.io>
2021-05-11 00:57:28 +0300

    fix

--- bot.py
+++ bot.py
@@ -36,11 +36,6 @@ def price(update, context):
     update.message.reply_text(f"🚀 {name} 🚀\n\n💰  1M tokens: ${round(price, 8)} \n💴  Market cap: ${market_cap}")
 
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -61,10 +56,6 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("piracy", piracy))
     dp.add_handler(CommandHandler("price", price))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
     # log all errors
     dp.add_error_handler(error)
 
