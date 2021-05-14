15788a9fd93d36fc93973dd0039baffa06e35e68
Cristian Ninicu <cristian@shoreline.io>
2021-05-14 11:49:34 +0300

    new token

--- bot.py
+++ bot.py
@@ -142,7 +142,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1838016774:AAEkm57AJJY8Iz1D_9P_z_8ifzb8qj8v1PU", use_context=True)
+    updater = Updater("1838016774:AAFt7lCU5IvHcu5NNXkwHhymcacwjME7iYI", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
